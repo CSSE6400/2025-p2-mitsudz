0dbea2af7082c33d3b79b311cbedb53b4111d9d8
mitsudz <MittunS2003@gmail.com>
2025-03-12 07:32:51 +0530

    Update init

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    from todo.models import db
+    from todo.models.todo import Todo
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
 
     from .views.routes import api
     app.register_blueprint(api)
